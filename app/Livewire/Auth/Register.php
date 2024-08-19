299f5f583d965936cccffa77f23e7764bb5d147f
Renato Kira <rntokira@gmail.com>
2024-08-18 21:20:34 -0300

    valitation rules to our registration form

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -4,16 +4,20 @@ namespace App\Livewire\Auth;
 
 use App\Models\User;
 use Illuminate\View\View;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Register extends Component
 {
+    #[Rule(['required', 'max:255'])]
     public ?string $name = null;
 
+    #[Rule(['required', 'email', 'max:255', 'confirmed'])]
     public ?string $email = null;
 
     public ?string $email_confirmation = null;
 
+    #[Rule(['required'])]
     public ?string $password = null;
 
     public function render(): View
@@ -23,6 +27,8 @@ class Register extends Component
 
     public function submit(): void
     {
+        $this->validate();
+
         User::query()->create([
             'name'     => $this->name,
             'email'    => $this->email,
--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -13,18 +13,35 @@ it('renders successfully', function () {
 it('should be able to  register new user in the system', function () {
 
     Livewire::test(Register::class)
-        ->set('name', 'Test User')
-        ->set('email', 'test@example.com')
-        ->set('email_confirmation', 'test@example.com')
+        ->set('name', 'Joe Doe')
+        ->set('email', 'joe@doe.com')
+        ->set('email_confirmation', 'joe@doe.com')
         ->set('password', 'secret')
         ->call('submit')
         ->assertHasNoErrors();
 
     assertDatabaseHas('users', [
-        'name'  => 'Test User',
-        'email' => 'test@example.com',
+        'name'  => 'Joe Doe',
+        'email' => 'joe@doe.com',
     ]);
 
     assertDatabaseCount('users', 1);
-
 });
+
+test('required fields', function ($field) {
+
+    Livewire::test(Register::class)
+        ->set($field->field, $field->value)
+        ->call('submit')
+        ->assertHasErrors([$field->field => $field->rule]);
+})->with([
+
+    'name::required'     => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
+    'name::max:255'      => (object)['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+    'email::required'    => (object)['field' => 'email', 'value' => '', 'rule' => 'required'],
+    'email::email'       => (object)['field' => 'email', 'value' => 'not-an-email', 'rule' => 'email'],
+    'email::max:255'     => (object)['field' => 'email', 'value' => str_repeat('*' . '@doe.com', 256), 'rule' => 'max'],
+    'email::confirmed'   => (object)['field' => 'email', 'value' => 'joe@doe.com', 'rule' => 'confirmed'],
+    'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
+
+]);
