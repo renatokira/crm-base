fe0d2261ef207ed49df73ec2dccd0bf577a460de
Renato Kira <rntokira@gmail.com>
2024-08-24 17:57:43 -0300

    user model has all methods to control permission for the given user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -29,5 +30,21 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password'          => 'hashed',
         ];
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function givePermissionTo(string $key)
+    {
+        $this->permissions()->firstOrCreate(compact('key'));
+    }
+
+    public function hasPermissionTo(string $key): bool
+    {
+
+        return $this->permissions()->where('key', $key)->exists();
     }
 }
