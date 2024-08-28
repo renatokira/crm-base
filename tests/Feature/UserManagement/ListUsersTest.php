888f5d91b8e52dd215b268f41c28c635996097c6
Renato Kira <rntokira@gmail.com>
2024-08-28 00:06:26 -0300

    making sure that the route is protected by permission BE_AN_ADMIN

--- tests/Feature/UserManagement/ListUsersTest.php
+++ tests/Feature/UserManagement/ListUsersTest.php
@@ -9,3 +9,11 @@ it('should be able to access the route admin/users', function () {
 
     actingAs($user)->get(route('admin.users'))->assertOk();
 });
+
+it('making sure that the route is protected by permission BE_AN_ADMIN', function () {
+
+    /** @var User $user */
+    $user = User::factory()->create();
+
+    actingAs($user)->get(route('admin.users'))->assertForbidden();
+});
