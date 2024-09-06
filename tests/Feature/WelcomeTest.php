5c5c992e0d163752fbd38ddf342c0c04ae2e7ebe
Renato Kira <rntokira@gmail.com>
2024-09-06 15:22:08 -0300

    be able list all matrices in the page welcome paginated

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -1,11 +1,32 @@
 <?php
 
 use App\Livewire\Welcome;
+use App\Models\{Matrix, User};
 use Livewire\Livewire;
+
+use function Pest\Laravel\actingAs;
 
 it('should render welcome component', function () {
 
     Livewire::test(Welcome::class)
         ->assertOk()
         ->assertViewIs('livewire.welcome');
+});
+
+it('should be able list all matrices in the page welcome paginated', function () {
+
+    /** @var User $user */
+    $user = User::factory()->create();
+    actingAs($user);
+
+    Matrix::factory()->count(15)->create();
+
+    Livewire::test(Welcome::class)
+        ->assertViewHas('matrices', function ($matrices) {
+            expect($matrices)
+                ->toBeInstanceOf(\Illuminate\Pagination\Paginator::class)
+                ->toHaveCount(10);
+
+            return true;
+        });
 });
